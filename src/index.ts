8836eb0fce9bae775a4118e69e7d678344ef5abf
noClaps <git@zerolimits.dev>
2025-06-18 00:46:44 +0100

    feat: Remove all color spaces other than OKLCH, RGB and hex

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Color from "colorjs.io";
 
-function color(color: string, format: string): string {
+function color(color: string, format: "hex" | "oklch" | "rgb"): string {
   try {
     switch (format) {
       case "hex":
@@ -9,8 +9,8 @@ function color(color: string, format: string): string {
       case "rgb":
         return new Color(color).to("srgb").toString();
 
-      default:
-        return new Color(color).to(format).toString();
+      case "oklch":
+        return new Color(color).to("oklch").toString();
     }
   } catch {
     return "ERROR: Invalid or unsupported input.";
@@ -22,7 +22,7 @@ function main() {
   const colorInput = document.querySelector("input")!;
   const output = document.querySelector("output")!;
 
-  const spaces = [...Object.keys(Color.spaces), "rgb", "hex"].sort();
+  const spaces = ["hex", "oklch", "rgb"] as const;
   for (const space of spaces) {
     formatSelect.insertAdjacentHTML(
       "beforeend",
@@ -30,15 +30,24 @@ function main() {
     );
   }
 
-  const outputColor = color(colorInput.value, formatSelect.value);
+  const outputColor = color(
+    colorInput.value,
+    formatSelect.value as "hex" | "oklch" | "rgb",
+  );
   output.innerText = outputColor;
 
   formatSelect.addEventListener("input", () => {
-    const outputColor = color(colorInput.value, formatSelect.value);
+    const outputColor = color(
+      colorInput.value,
+      formatSelect.value as "hex" | "oklch" | "rgb",
+    );
     output.innerText = outputColor;
   });
   colorInput.addEventListener("input", () => {
-    const outputColor = color(colorInput.value, formatSelect.value);
+    const outputColor = color(
+      colorInput.value,
+      formatSelect.value as "hex" | "oklch" | "rgb",
+    );
     output.innerText = outputColor;
   });
 }
