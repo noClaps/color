ac991e7a1f5d43d9e671a9163d9853774dba16f4
noClaps <git@zerolimits.dev>
2025-06-20 17:25:30 +0100

    chore: Move `closeEnough()` function to `rgb.go`

--- internal/color/rgb.go
+++ internal/color/rgb.go
@@ -171,3 +171,10 @@ func (rgb RGBA) String() string {
 	}
 	return fmt.Sprintf("rgb(%v %v %v%s)", rgb.Red, rgb.Green, rgb.Blue, alpha)
 }
+
+func closeEnough(f float64) float64 {
+	if math.Abs(math.Round(f)-f) < 1e-5 {
+		return math.Round(f)
+	}
+	return f
+}
